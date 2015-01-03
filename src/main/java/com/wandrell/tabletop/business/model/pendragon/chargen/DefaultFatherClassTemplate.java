4e8ac525fffb6ac8bc89d314d7ec2a36756804d6
Bernardo Martínez Garrido <programming@wandrell.com>
2015-01-03 01:06:19 +0100

    Corrected the Father Class Template.

--- src/main/java/com/wandrell/tabletop/business/model/pendragon/chargen/DefaultFatherClassTemplate.java
+++ src/main/java/com/wandrell/tabletop/business/model/pendragon/chargen/DefaultFatherClassTemplate.java
@@ -4,42 +4,65 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Objects;
 
 import com.google.common.base.MoreObjects;
-import com.wandrell.tabletop.business.model.pendragon.inventory.Money;
+import com.wandrell.tabletop.business.model.dice.Dice;
+import com.wandrell.tabletop.business.model.skill.NameAndDescriptor;
 
 public final class DefaultFatherClassTemplate implements FatherClassTemplate {
 
-    private final Money              money;
-    private final String             name;
-    private final Collection<String> skillsGroup;
-    private final Integer            skillsGroupPoints;
-    private final Integer            skillsNonCombatPoints;
-    private final Integer            skillsPoints;
+    private final Map<NameAndDescriptor, Integer> directedTraits;
+    private final Map<NameAndDescriptor, Integer> directedTraitsBase;
+    private final Dice                            money;
+    private final String                          name;
+    private final Collection<NameAndDescriptor>   skillsGroup;
+    private final Integer                         skillsGroupPointsBonus;
+    private final Integer                         skillsGroupPointsDivide;
+    private final Integer                         skillsNonCombatPoints;
+    private final Integer                         skillsPoints;
+    private final Map<String, Integer>            specialtySkills;
 
     public DefaultFatherClassTemplate(final String name,
             final Integer skillsGroupPoints,
-            final Collection<String> skillsGroup, final Integer skillsPoints,
-            final Integer skillsNonCombatPoints, final Money money) {
+            final Integer skillsGroupPointsDivide, final Integer skillsPoints,
+            final Integer skillsNonCombatPoints, final Dice money,
+            final Collection<NameAndDescriptor> skillsGroup,
+            final Map<String, Integer> specialtySkills,
+            final Map<NameAndDescriptor, Integer> directedTraits,
+            final Map<NameAndDescriptor, Integer> directedTraitsBase) {
         super();
 
         checkNotNull(name, "Received a null pointer as name");
-        checkNotNull(skillsGroupPoints,
-                "Received a null pointer as skills group points");
-        checkNotNull(skillsGroup, "Received a null pointer as skills group");
         checkNotNull(skillsPoints, "Received a null pointer as skills points");
         checkNotNull(skillsNonCombatPoints,
                 "Received a null pointer as non combat skills points");
         checkNotNull(money, "Received a null pointer as money");
+        checkNotNull(skillsGroupPoints,
+                "Received a null pointer as skills group points to add");
+        checkNotNull(skillsGroupPointsDivide,
+                "Received a null pointer as skills group points to divide");
+        checkNotNull(skillsGroup, "Received a null pointer as skills group");
+        checkNotNull(directedTraits,
+                "Received a null pointer as directed traits bonus");
+        checkNotNull(directedTraitsBase,
+                "Received a null pointer as directed traits base values");
+        checkNotNull(specialtySkills,
+                "Received a null pointer as specialty skills");
 
         this.name = name;
 
         this.money = money;
 
-        this.skillsGroupPoints = skillsGroupPoints;
+        this.skillsGroupPointsBonus = skillsGroupPoints;
+        this.skillsGroupPointsDivide = skillsGroupPointsDivide;
 
         this.skillsGroup = skillsGroup;
+        this.specialtySkills = specialtySkills;
+
+        this.directedTraits = directedTraits;
+        this.directedTraitsBase = directedTraitsBase;
 
         this.skillsPoints = skillsPoints;
 
@@ -59,33 +82,53 @@ public final class DefaultFatherClassTemplate implements FatherClassTemplate {
     }
 
     @Override
-    public final String getFatherClass() {
-        return name;
+    public final Map<NameAndDescriptor, Integer> getDirectedTraits() {
+        return Collections.unmodifiableMap(getDirectedTraitsModifiable());
     }
 
     @Override
-    public final Money getMoney() {
+    public final Map<NameAndDescriptor, Integer> getDirectedTraitsBase() {
+        return Collections.unmodifiableMap(getDirectedTraitsBaseModifiable());
+    }
+
+    @Override
+    public final Dice getMoney() {
         return money;
     }
 
     @Override
-    public final Integer getNonCombatSkillsPoints() {
+    public final String getName() {
+        return name;
+    }
+
+    @Override
+    public final Integer getNonCombatSkillBonus() {
         return skillsNonCombatPoints;
     }
 
     @Override
-    public final Collection<String> getSkillsGroup() {
+    public final Collection<NameAndDescriptor> getSkillsGroup() {
         return Collections.unmodifiableCollection(getSkillsGroupModifiable());
     }
 
     @Override
-    public final Integer getSkillsGroupPoints() {
-        return skillsGroupPoints;
+    public final Integer getSkillsGroupBonusPoints() {
+        return skillsGroupPointsBonus;
+    }
+
+    @Override
+    public final Integer getSkillsGroupDividePoints() {
+        return skillsGroupPointsDivide;
     }
 
     @Override
     public final Integer getSkillsPoints() {
         return skillsPoints;
+    }
+
+    @Override
+    public final Map<String, Integer> getSpecialtySkills() {
+        return Collections.unmodifiableMap(getSpecialtySkillsModifiable());
     }
 
     @Override
@@ -98,8 +141,21 @@ public final class DefaultFatherClassTemplate implements FatherClassTemplate {
         return MoreObjects.toStringHelper(this).add("name", name).toString();
     }
 
-    private final Collection<String> getSkillsGroupModifiable() {
+    private final Map<NameAndDescriptor, Integer>
+            getDirectedTraitsBaseModifiable() {
+        return directedTraitsBase;
+    }
+
+    private final Map<NameAndDescriptor, Integer> getDirectedTraitsModifiable() {
+        return directedTraits;
+    }
+
+    private final Collection<NameAndDescriptor> getSkillsGroupModifiable() {
         return skillsGroup;
+    }
+
+    private final Map<String, Integer> getSpecialtySkillsModifiable() {
+        return specialtySkills;
     }
 
 }
