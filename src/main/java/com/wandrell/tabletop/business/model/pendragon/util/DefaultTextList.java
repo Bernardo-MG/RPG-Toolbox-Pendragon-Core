0e21a39d521f8cdba44dbe54bc2fb3919d7443e8
Bernardo Martínez Garrido <programming@wandrell.com>
2015-01-09 17:24:45 +0100

    Added a class for storing text values lists.

---
+++ src/main/java/com/wandrell/tabletop/business/model/pendragon/util/DefaultTextList.java
@@ -0,0 +1,58 @@
+package com.wandrell.tabletop.business.model.pendragon.util;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import com.google.common.base.MoreObjects;
+
+public final class DefaultTextList implements TextList {
+
+    private final String             listName;
+    private final Collection<String> listValues;
+
+    public DefaultTextList(final String name, final Collection<String> values) {
+        super();
+
+        checkNotNull(name, "Received a null pointer as name");
+        checkNotNull(values, "Received a null pointer as values");
+
+        listName = name;
+        listValues = values;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DefaultTextList other = (DefaultTextList) obj;
+        return Objects.equals(listName, other.listName);
+    }
+
+    @Override
+    public final String getName() {
+        return listName;
+    }
+
+    @Override
+    public final Collection<String> getValues() {
+        return listValues;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(listName);
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("name", listName)
+                .toString();
+    }
+
+}
