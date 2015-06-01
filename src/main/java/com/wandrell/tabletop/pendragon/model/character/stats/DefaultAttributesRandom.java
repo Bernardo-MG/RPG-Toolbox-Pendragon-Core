db2ac8c128cbb32a35c13dcae46fe258161ed6be
Bernardo Martínez Garrido <programming@wandrell.com>
2015-06-01 18:20:02 +0200

    Style fix.

--- src/main/java/com/wandrell/tabletop/pendragon/model/character/stats/DefaultAttributesRandom.java
+++ src/main/java/com/wandrell/tabletop/pendragon/model/character/stats/DefaultAttributesRandom.java
@@ -35,13 +35,20 @@ public final class DefaultAttributesRandom implements AttributesRandom {
 
     @Override
     public final boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+
+        if (getClass() != obj.getClass()) {
             return false;
+        }
+
         DefaultAttributesRandom other = (DefaultAttributesRandom) obj;
+
         return Objects.equals(appearance, other.appearance)
                 && Objects.equals(constitution, other.constitution)
                 && Objects.equals(dexterity, other.dexterity)
